485b271d511548f5e8e4eac876226acf19e52594
flywukong <2229306838@qq.com>
2024-08-07 15:01:32 +0800

    support heap pprof

--- perf/main.go
+++ perf/main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"net/http/pprof"
+	_ "net/http/pprof"
 	"os"
 	"path/filepath"
 	"time"
@@ -213,8 +213,10 @@ func runPerfDB(c *cli.Context) error {
 	fmt.Println("Enabling stand-alone metrics HTTP endpoint", "address", address)
 	exp.Setup(address)
 
-	http.HandleFunc("/debug/pprof/heap", pprof.Index)
-	http.ListenAndServe(":80", nil)
+	//http.HandleFunc("/debug/pprof/heap", pprof.Index)
+	//http.ListenAndServe(":80", nil)
+
+	_ = http.ListenAndServe("0.0.0.0:6061", nil)
 	go metrics.CollectProcessMetrics(3 * time.Second)
 	runner.Run(ctx)
 	return nil
